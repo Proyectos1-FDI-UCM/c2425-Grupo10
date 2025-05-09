a60110956f93f1a4a07eec29edfd505865eb031f
Juliiaa24 <jvera02@ucm.es>
2025-05-09 02:17:45 +0200

    Scripts Inventario
    Variables Cambiadas
    Organización Correcta
    Nuevas regiones
    Serialized quitados

--- Assets/Scripts/Managers/InventoryManager.cs
+++ Assets/Scripts/Managers/InventoryManager.cs
@@ -1,5 +1,5 @@
 //---------------------------------------------------------
-// Script para gestionar los datos del invantario
+// Script para gestionar los datos del inventario
 // Responsable: Julia Vera Ruiz
 // Nombre del juego: Roots of Life
 // Curso 2024-25
@@ -45,34 +45,24 @@ public static class InventoryManager
     /// </summary>
     private static int[] Inventory = new int[(int)Items.Count];
 
-    /// <summary>
-    /// Capacidad Máxima
+    /// <summary> 
+    /// Capacidad Máxima Semillas
     /// </summary>
-    [SerializeField] private static int MaxSeedQuantity = 30; // Cantidad máxima de espacio disponible en el inventory para las semillas
-    [SerializeField] private static int MaxCropQuantity = 40; // Cantidad máxima de espacio disponible en el inventory para los cultivos
+    private static int MaxSeedQuantity = 30; // Cantidad máxima de espacio disponible en el inventory para las semillas
 
+    /// <summary>
+    /// Capacidad Máxima Cultivos
+    /// </summary>
+    private static int MaxCropQuantity = 40; // Cantidad máxima de espacio disponible en el inventory para los cultivos
+
+    /// <summary>
+    /// Bool que se activa si el inventario está lleno para algún elemento
+    /// </summary>
     private static bool _inventoryFull = false;
 
 
-    public static void SetPlayerPosition(Vector3 position)
-    {
-        PlayerPosition = position;
-    }
-
-    public static Vector3 GetPlayerPosition()
-    {
-        return PlayerPosition;
-    }
-
-    public static int[] GetInventory()
-    {
-        return Inventory;
-    }
-
-    public static void SetInventory(int[] inventory)
-    {
-        Inventory = inventory;
-    }
+    // ---- MÉTODOS PÚBLICOS ----
+    #region Métodos Públicos
 
     public static void ResetInventory()
     {
@@ -82,174 +72,219 @@ public static class InventoryManager
         }
     }
 
-    /// <summary>
-    /// Devuelve un entero, la cantidad de dicho item que tiene el jugador
-    /// </summary>
-    public static int GetInventoryItem(Items item)  
-    {
-        return Inventory[(int)item];
-    }
-
-    public static int GetInventoryItem(int item)
-    {
-        return Inventory[(int)item];
-    }
-
-    public static void ModifyPlayerPosition(Vector3 position)
-    {
-        PlayerPosition = position;
-    }
-
-
-    /// <summary>
-    /// Devuelve True si se efectua la modificación
-    /// Añade la cantidad (quantity) al inventory del Item (item) comprobando que los valores esten dentro de los parámetros permitidos
-    /// </summary>
-    public static bool BoolModifyInventory(Items item, int quantity)
-    {
-        if ((int)item >= (int)Items.Count / 2) // Es un cultivo 
+        // ---- MÉTODOS SET ----
+        #region Métodos Set
+        /// <summary>
+        /// Modifica la Posición del Jugador
+        /// </summary>
+        /// <param name="position"></param>
+        public static void SetPlayerPosition(Vector3 position)
         {
-            if (Inventory[(int)item] + quantity <= MaxCropQuantity)
+            PlayerPosition = position;
+        }
+        public static void ModifyPlayerPosition(Vector3 position)
+        {
+            PlayerPosition = position;
+        }
+
+        /// <summary>
+        /// Establece el inventario completo (Cargar Partida)
+        /// </summary>
+        /// <param name="inventory"></param>
+        public static void SetInventory(int[] inventory)
+        {
+            Inventory = inventory;
+        }
+
+        /// <summary>
+        /// Cambia la cantidad máxima de cultivos que puedes guardar en el inventory
+        /// </summary>
+        public static void SetMaxCrops(int maxCrops)
+        {
+            MaxCropQuantity = maxCrops;
+        }
+
+        /// <summary>
+        /// Cambia la cantidad máxima de semillas que puedes guardar en el inventory
+        /// </summary>
+        public static void SetMaxSeeds(int maxSeeds)
+        {
+            MaxSeedQuantity = maxSeeds;
+        }
+
+        #endregion
+
+        // ---- MÉTODOS GET ----
+        #region Métodos Get
+
+        /// <summary>
+        /// Devuelve un entero, la cantidad de dicho item que tiene el jugador
+        /// </summary>
+        public static int GetInventoryItem(Items item)
+        {
+            return Inventory[(int)item];
+        }
+
+        public static int GetInventoryItem(int item)
+        {
+            return Inventory[(int)item];
+        }
+
+        /// <summary>
+        /// Guarda la Posición del Jugador
+        /// </summary>
+        /// <returns></returns>
+        public static Vector3 GetPlayerPosition()
+        {
+            return PlayerPosition;
+        }
+
+        public static int[] GetInventory()
+        {
+            return Inventory;
+        }
+
+        ///<summary>
+        /// Metodo para cambiar el bool del inventario
+        /// </summary>
+        public static void InventoryNotFull()
+        {
+            _inventoryFull = false;
+        }
+
+        ///<summary>
+        ///metodo para saber si el inventario esta lleno
+        /// </summary>
+        public static bool GetBoolInventoryFull()
+        {
+            return _inventoryFull;
+        }
+
+        /// <summary>
+        /// Devuelve el máximo de semillas que se pueden guardar en el inventory
+        /// </summary>
+        public static int GetMaxSeeds()
+        {
+            return MaxSeedQuantity;
+        }
+
+        /// <summary>
+        /// Devuelve el máximo de cultivos que se pueden guardar en el inventory
+        /// (Para mostrar slots en el inventory)
+        /// </summary>
+        public static int GetMaxCrop()
+        {
+            return MaxCropQuantity;
+        }
+
+        #endregion
+
+        // ---- MÉTODOS INVENTARIO ----
+        #region Métodos Inventario
+
+        /// <summary>
+        /// Devuelve True si se efectua la modificación
+        /// Añade la cantidad (quantity) al inventory del Item (item) comprobando que los valores esten dentro de los parámetros permitidos
+        /// </summary>
+        public static bool BoolModifyInventory(Items item, int quantity)
+        {
+            if ((int)item >= (int)Items.Count / 2) // Es un cultivo 
             {
-                Inventory[(int)item] += quantity;
+                if (Inventory[(int)item] + quantity <= MaxCropQuantity)
+                {
+                    Inventory[(int)item] += quantity;
+                    return true;
+                }
+                else 
+                {
+                    Debug.Log("InventarioLleno");
+                    _inventoryFull = true;
+                }
+                return false;
+            }
+            else // Es una semilla
+            {
+                if (Inventory[(int)item] + quantity <= MaxSeedQuantity)
+                {
+                    Inventory[(int)item] += quantity;
+                    return true;
+                }
+                else
+                {
+                    Debug.Log("InventarioLleno");
+                    _inventoryFull = true;
+                }
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Devuelve True si se efectua la modificación
+        /// Resta la cantidad (quantity) al inventory del Item (item) comprobando que los valores esten dentro de los parámetros permitidos 
+        /// IMPORTANTE - USAR NUMEROS POSITIVOS (ModifyInventorySubstract(Item.Corn, 5) - Resta 5 Maices)
+        /// </summary>
+        public static bool BoolModifyInventorySubstract(Items item, int quantity) // (Se puede restar con números negativos)
+        {
+            if ((int)item >= (int)Items.Count / 2) // Es un cultivo 
+            {
+                if (Inventory[(int)item] - quantity <= 0) Inventory[(int)item] -= quantity;
+                else Debug.Log("InventarioInsuficiente");
                 return true;
             }
-            else 
+            else // Es una semilla
             {
-                Debug.Log("InventarioLleno");
-                _inventoryFull = true;
+                if (Inventory[(int)item] - quantity <= 0) Inventory[(int)item] -= quantity;
+                else Debug.Log("InventarioInsuficiente");
+                return false;
             }
-            return false;
         }
-        else // Es una semilla
+
+        /// <summary>
+        /// Añade la cantidad (quantity) al inventory del Item (item) comprobando que los valores esten dentro de los parámetros permitidos
+        /// </summary>
+        public static void ModifyInventory(Items item, int quantity)
         {
-            if (Inventory[(int)item] + quantity <= MaxSeedQuantity)
+            if ((int)item >= (int)Items.Count / 2) // Es un cultivo 
             {
-                Inventory[(int)item] += quantity;
-                return true;
+                if (Inventory[(int)item] + quantity <= MaxCropQuantity) Inventory[(int)item] += quantity;
+                else
+                {
+                    Debug.Log("InventarioLleno");
+                    _inventoryFull = true;
+                }
+                Debug.Log("Item" + item.ToString() + " Cantidad: " + quantity);
             }
-            else
+            else // Es una semilla
             {
-                Debug.Log("InventarioLleno");
-                _inventoryFull = true;
+                if (Inventory[(int)item] + quantity <= MaxSeedQuantity) Inventory[(int)item] += quantity;
+                else
+                {
+                    Debug.Log("InventarioLleno");
+                    _inventoryFull = true;
+                }
+                Debug.Log("Item" + item.ToString() + " Cantidad: " + quantity);
             }
-            return false;
         }
-    }
 
-    /// <summary>
-    /// Devuelve True si se efectua la modificación
-    /// Resta la cantidad (quantity) al inventory del Item (item) comprobando que los valores esten dentro de los parámetros permitidos 
-    /// IMPORTANTE - USAR NUMEROS POSITIVOS (ModifyInventorySubstract(Item.Corn, 5) - Resta 5 Maices)
-    /// </summary>
-    public static bool BoolModifyInventorySubstract(Items item, int quantity) // (Se puede restar con números negativos)
-    {
-        if ((int)item >= (int)Items.Count / 2) // Es un cultivo 
+        /// <summary>
+        /// Resta la cantidad (quantity) al inventory del Item (item) comprobando que los valores esten dentro de los parámetros permitidos 
+        /// IMPORTANTE - USAR NUMEROS POSITIVOS (ModifyInventorySubstract(Item.Corn, 5) - Resta 5 Maices)
+        /// </summary>
+        public static void ModifyInventorySubstract(Items item, int quantity) // (Se puede restar con números negativos)
         {
-            if (Inventory[(int)item] - quantity <= 0) Inventory[(int)item] -= quantity;
-            else Debug.Log("InventarioInsuficiente");
-            return true;
-        }
-        else // Es una semilla
-        {
-            if (Inventory[(int)item] - quantity <= 0) Inventory[(int)item] -= quantity;
-            else Debug.Log("InventarioInsuficiente");
-            return false;
-        }
-    }
-
-    /// <summary>
-    /// Añade la cantidad (quantity) al inventory del Item (item) comprobando que los valores esten dentro de los parámetros permitidos
-    /// </summary>
-    public static void ModifyInventory(Items item, int quantity)
-    {
-        if ((int)item >= (int)Items.Count / 2) // Es un cultivo 
-        {
-            if (Inventory[(int)item] + quantity <= MaxCropQuantity) Inventory[(int)item] += quantity;
-            else
+            if ((int)item >= (int)Items.Count / 2) // Es un cultivo 
             {
-                Debug.Log("InventarioLleno");
-                _inventoryFull = true;
+                if (Inventory[(int)item] - quantity >= 0) Inventory[(int)item] -= quantity;
+                else Debug.Log("InventarioInsuficiente");
             }
-            Debug.Log("Item" + item.ToString() + " Cantidad: " + quantity);
-        }
-        else // Es una semilla
-        {
-            if (Inventory[(int)item] + quantity <= MaxSeedQuantity) Inventory[(int)item] += quantity;
-            else
+            else // Es una semilla
             {
-                Debug.Log("InventarioLleno");
-                _inventoryFull = true;
+                if (Inventory[(int)item] - quantity >= 0) Inventory[(int)item] -= quantity;
+                else Debug.Log("InventarioInsuficiente");
             }
-            Debug.Log("Item" + item.ToString() + " Cantidad: " + quantity);
         }
-    }
 
-    /// <summary>
-    /// Resta la cantidad (quantity) al inventory del Item (item) comprobando que los valores esten dentro de los parámetros permitidos 
-    /// IMPORTANTE - USAR NUMEROS POSITIVOS (ModifyInventorySubstract(Item.Corn, 5) - Resta 5 Maices)
-    /// </summary>
-    public static void ModifyInventorySubstract(Items item, int quantity) // (Se puede restar con números negativos)
-    {
-        if ((int)item >= (int)Items.Count / 2) // Es un cultivo 
-        {
-            if (Inventory[(int)item] - quantity >= 0) Inventory[(int)item] -= quantity;
-            else Debug.Log("InventarioInsuficiente");
-        }
-        else // Es una semilla
-        {
-            if (Inventory[(int)item] - quantity >= 0) Inventory[(int)item] -= quantity;
-            else Debug.Log("InventarioInsuficiente");
-        }
-    }
+            #endregion // métodos modificar inventario
 
-
-    /// <summary>
-    /// Cambia la cantidad máxima de cultivos que puedes guardar en el inventory
-    /// </summary>
-    public static void SetMaxCrops (int maxCrops)
-    {
-        MaxCropQuantity = maxCrops;
-    }
-
-    /// <summary>
-    /// Cambia la cantidad máxima de semillas que puedes guardar en el inventory
-    /// </summary>
-    public static void SetMaxSeeds(int maxSeeds)
-    {
-        MaxSeedQuantity = maxSeeds;
-    }
-
-    /// <summary>
-    /// Devuelve el máximo de cultivos que se pueden guardar en el inventory
-    /// (Para mostrar slots en el inventory)
-    /// </summary>
-    public static int GetMaxCrop()
-    {
-        return MaxCropQuantity;
-    }
-    ///<summary>
-    /// Metodo para cambiar el bool del inventario
-    /// </summary>
-    public static void InventoryNotFull()
-    {
-        _inventoryFull = false;
-    }
-
-    ///<summary>
-    ///metodo para saber si el inventario esta lleno
-    /// </summary>
-    public static bool GetBoolInventoryFull()
-    {
-        return _inventoryFull;
-    }
-
-    /// <summary>
-    /// Devuelve el máximo de semillas que se pueden guardar en el inventory
-    /// </summary>
-    public static int GetMaxSeeds()
-    {
-        return MaxSeedQuantity;
-    }
-
+    #endregion // métodos públicos
 }
